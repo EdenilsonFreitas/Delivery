02943d6147f794929b2b6606bf5b078b6615bfcf
Edenilson <freitaseddie126@gmail.com>
2024-08-29 18:28:47 -0300

    Criando o Layout Principal

--- app/Views/Admin/Home/index.php
+++ app/Views/Admin/Home/index.php
@@ -7,16 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Majestic Admin</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="vendors/base/vendor.bundle.base.css">
+    <link rel="stylesheet" href="<?php echo site_url('admin/');?>vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="<?php echo site_url('admin/');?>vendors/base/vendor.bundle.base.css">
     <!-- endinject -->
     <!-- plugin css for this page -->
-    <link rel="stylesheet" href="vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+    <link rel="stylesheet" href="<?php echo site_url('admin/');?>vendors/datatables.net-bs4/dataTables.bootstrap4.css">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="<?php echo site_url('admin/');?>css/style.css">
     <!-- endinject -->
-    <link rel="shortcut icon" href="images/favicon.png" />
+    <link rel="shortcut icon" href="<?php echo site_url('admin/');?>images/favicon.png" />
 </head>
 
 <body>
@@ -641,25 +641,25 @@
     <!-- container-scroller -->
 
     <!-- plugins:js -->
-    <script src="vendors/base/vendor.bundle.base.js"></script>
+    <script src="<?php echo site_url('admin/');?>vendors/base/vendor.bundle.base.js"></script>
     <!-- endinject -->
     <!-- Plugin js for this page-->
-    <script src="vendors/chart.js/Chart.min.js"></script>
-    <script src="vendors/datatables.net/jquery.dataTables.js"></script>
-    <script src="vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+    <script src="<?php echo site_url('admin/');?>vendors/chart.js/Chart.min.js"></script>
+    <script src="<?php echo site_url('admin/');?>vendors/datatables.net/jquery.dataTables.js"></script>
+    <script src="<?php echo site_url('admin/');?>vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
     <!-- End plugin js for this page-->
     <!-- inject:js -->
-    <script src="js/off-canvas.js"></script>
-    <script src="js/hoverable-collapse.js"></script>
-    <script src="js/template.js"></script>
+    <script src="<?php echo site_url('admin/');?>js/off-canvas.js"></script>
+    <script src="<?php echo site_url('admin/');?>js/hoverable-collapse.js"></script>
+    <script src="<?php echo site_url('admin/');?>js/template.js"></script>
     <!-- endinject -->
     <!-- Custom js for this page-->
-    <script src="js/dashboard.js"></script>
-    <script src="js/data-table.js"></script>
-    <script src="js/jquery.dataTables.js"></script>
-    <script src="js/dataTables.bootstrap4.js"></script>
+    <script src="<?php echo site_url('admin/');?>js/dashboard.js"></script>
+    <script src="<?php echo site_url('admin/');?>js/data-table.js"></script>
+    <script src="<?php echo site_url('admin/');?>js/jquery.dataTables.js"></script>
+    <script src="<?php echo site_url('admin/');?>js/dataTables.bootstrap4.js"></script>
     <!-- End custom js for this page-->
-    <script src="js/jquery.cookie.js" type="text/javascript"></script>
+    <script src="<?php echo site_url('admin/');?>js/jquery.cookie.js" type="text/javascript"></script>
 </body>
 
 </html>
