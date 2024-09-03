bae0f619b4eec762d8e8202cfa9a2351fb0eb884
Edenilson <freitaseddie126@gmail.com>
2024-09-03 20:56:23 -0300

    Criando o banco de Dados parte 1

---
+++ app/Database/Migrations/2024-09-03-225918_CriaTabelaUsuarios.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CriaTabelaUsuarios extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'=>[
+                'type'=>'INT',
+                'constraint'=>5,
+                'unsigned'=>true,
+                'auto_increment'=>true,
+            ],
+            'name'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'128'
+            ],
+            'email'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'255'
+            ],
+            'cpf'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'15'
+            ],
+            'telefone'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'20'
+            ],
+            'is_admin'=>[
+                'type'=>'BOOLEAN',
+                'null'=>'false',
+                'default'=>'false',
+            ],
+            'ativo'=>[
+                'type'=>'BOOLEAN',
+                'null'=>'false',
+                'default'=>'false',
+            ],
+            'password_hash'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'255',
+            ],
+            'ativacao_hash'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'255',
+            ],
+            'reset_hash'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>'255',
+            ],
+            'reset_expira_em'=>[
+                'type'=>'DATETIME',
+                'null'=> true,
+                'default'=> null,
+            ],
+            'criado_em'=>[
+                'type'=>'DATETIME',
+                'null'=> true,
+                'default'=> null,
+            ],
+            'atualizado_em'=>[
+                'type'=>'DATETIME',
+                'null'=> true,
+                'default'=> null,
+            ],
+            'deletado_em'=>[
+                'type'=>'DATETIME',
+                'null'=> true,
+                'default'=> null,
+            ],
+
+
+        ]);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
