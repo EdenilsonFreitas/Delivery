07bd35b01dd894390f514f5d364b3ba129531260
Edenilson <freitaseddie126@gmail.com>
2024-09-10 17:28:35 -0300

    Instalando o plugin autocomplete para listar os usuarios

--- app/Views/Admin/Usuarios/index.php
+++ app/Views/Admin/Usuarios/index.php
@@ -7,6 +7,10 @@
 
 <?php echo $this->section('estilos'); ?>
 <!--Aqui enviamos para o templete principal os estilios-->
+
+<link rel="stylesheet" href="<?php echo site_url('admin/vendors/auto-complete/jquery-ui.css');?>" />
+
+
 <?php echo $this->endSection(); ?>
 
 
@@ -17,12 +21,16 @@
 <?php echo $this->section('conteudo'); ?>
 
 <div class="row">
-    
+
     <div class="col-lg-12 grid-margin stretch-card">
         <div class="card">
             <div class="card-body">
                 <h4 class="card-title"><?php echo $titulo; ?></h4>
-                
+
+                <div class="ui-widget">
+                    <input id="query" name="query" class="form-control bg-light mb-5">
+                </div>
+
                 <div class="table-responsive">
                     <table class="table table-hover">
                         <thead>
@@ -41,13 +49,14 @@
                                 <td><?php echo $usuario->nome; ?></td>
                                 <td><?php echo $usuario->email; ?></td>
                                 <td><?php echo $usuario->cpf; ?></td>
-                                <td> <?php echo ($usuario-> ativo ? '<label class="badge badge-primary">Sim</label>' : '<label class="badge badge-danger">Não</label>'); ?> </td>
-                               
-                                
+                                <td> <?php echo ($usuario-> ativo ? '<label class="badge badge-primary">Sim</label>' : '<label class="badge badge-danger">Não</label>'); ?>
+                                </td>
+
+
                             </tr>
-                            
+
                             <?php endforeach; ?>
-                         
+
                         </tbody>
                     </table>
                 </div>
@@ -62,5 +71,55 @@
 
 <?php echo $this->section('scripts'); ?>
 <!--Aqui enviamos para o templete principal os scripts-->
+
+<script src="<?php echo site_url('admin/vendors/auto-complete/jquery-ui.js'); ?>"></script>
+
+
+<script>
+
+    $(function(){
+
+        $( "#query" ).autocomplete({
+      source: function (request, response){
+        $.ajax({
+
+            url: "<?php echo site_url('admin/usuarios/procurar'); ?>",
+            dataType: "json",
+            data:{
+                term:request.term
+            },
+            success: function(data){
+                if(data.length < 1){
+                    var data = [
+                        {
+                            label: 'Usuário não encontrado',
+                            value: -1
+                        }
+                    ];
+
+                }
+                response(data);// aqui temos o valor no data
+
+            },
+
+        }); // fim do ajax
+
+      },
+      minLenght: 1,
+      select:function(event, ui){
+        if(ui.item.value == -1){
+            $(this).val("");
+            return false;
+        }else{
+            window.location.href='<?php echo site_url('admin/usuarios/show/'); ?>' + ui.item.id;
+        }
+
+      }
+    }); // fim do autocomplete
+        
+    });                            
+
+
+</script>
 
 <?php echo $this->endSection(); ?>
