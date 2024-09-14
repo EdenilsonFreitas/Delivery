e640853ba0ecf5ed5f9fb343eaa025d38b247d5c
Edenilson <freitaseddie126@gmail.com>
2024-09-14 20:18:58 -0300

    Editando o usuário parte 03

--- app/Views/Admin/Usuarios/form.php
+++ app/Views/Admin/Usuarios/form.php
@@ -1,24 +1,25 @@
 <div class="form-row">
 
-<div class="form-group col-md-4">
-    <label for="nome">Nome</label>
-    <input type="text" class="form-control" name="nome" id="nome" value="<?php echo $usuario->nome;?>" >
-</div>
+    <div class="form-group col-md-4">
+        <label for="nome">Nome</label>
+        <input type="text" class="form-control" name="nome" id="nome" value="<?php echo esc($usuario->nome);?>">
+    </div>
 
-<div class="form-group col-md-2">
-    <label for="cpf">CPF</label>
-    <input type="text" class="form-control cpf" name="cpf" id="cpf" value="<?php echo $usuario->cpf;?>" >
-</div>
+    <div class="form-group col-md-2">
+        <label for="cpf">CPF</label>
+        <input type="text" class="form-control cpf" name="cpf" id="cpf" value="<?php echo esc($usuario->cpf);?>">
+    </div>
 
-<div class="form-group col-md-3">
-    <label for="telefone">Telefone</label>
-    <input type="text" class="form-control sp_celphones" name="telefone" id="telefone" value="<?php echo $usuario->telefone; ?>" >
-</div>
+    <div class="form-group col-md-3">
+        <label for="telefone">Telefone</label>
+        <input type="text" class="form-control sp_celphones" name="telefone" id="telefone"
+            value="<?php echo esc($usuario->telefone); ?>">
+    </div>
 
-<div class="form-group col-md-3">
-    <label for="email">E-mail</label>
-    <input type="email" class="form-control" name="email" id="email" value="<?php echo $usuario->email; ?>" >
-</div>
+    <div class="form-group col-md-3">
+        <label for="email">E-mail</label>
+        <input type="email" class="form-control" name="email" id="email" value="<?php echo esc($usuario->email); ?>">
+    </div>
 
 
 </div>
@@ -27,21 +28,67 @@
 <div class="form-row">
 
 
-<div class="form-group col-md-3">
-    <label for="password">Senha</label>
-    <input type="password" class="form-control" name="password" id="password" >
+    <div class="form-group col-md-3">
+        <label for="password">Senha</label>
+        <input type="password" class="form-control" name="password" id="password">
+    </div>
+
+    <div class="form-group col-md-3">
+        <label for="confirmation_password">Confirmação de Senha</label>
+        <input type="password" class="form-control" name="password_confirmation" id="confirmation_password">
+
+    </div>
+
+    <div class="form-group col-md-3">
+        <label for="email">Perfil de acesso</label>
+        <select class="form-control" name="is_admin">
+
+            <?php if($usuario->id):?>
+            <option value="1" <?php echo ($usuario-> is_admin? 'selected' : ''); ?>>Administrador</option>
+            <option value="0" <?php echo (!$usuario-> is_admin ? 'selected' : ''); ?>>Cliente</option>
+            
+
+            <?php else: ?>
+            <option value="1">Sim</option>
+            <option value="0" selected="">Não</option>
+
+            <?php endif; ?>
+
+
+        </select>
+
+    </div>
+
+    <div class="form-group col-md-3">
+        <label for="email">Ativo</label>
+        <select class="form-control" name="ativo">
+
+            <?php if($usuario->id):?>
+            <option value="1" <?php echo ($usuario-> ativo ? 'selected' : ''); ?>>Sim</option>
+            <option value="0" <?php echo (!$usuario-> ativo ? 'selected' : ''); ?>>Não</option>
+            
+
+            <?php else: ?>
+            <option value="1">Sim</option>
+            <option value="0" selected="">Não</option>
+
+            <?php endif; ?>
+
+
+        </select>
+
+    </div>
+
 </div>
 
-<div class="form-group col-md-3">
-    <label for="confirmation_password">Confirmação de Senha</label>
-    <input type="password" class="form-control" name="confirmation_password" id="confirmation_password" >
-</div>
-
-</div>
 
 
 
 
+<button type="submit" class="btn btn-primary">
+    <i class="mdi mdi-floppy mr-2"></i>Salvar
+</button>
 
-<button type="submit" class="btn btn-primary mr-2">Salvar</button>
- <a href="<?= site_url("admin/usuarios/show/{$usuario->id}"); ?>" class="btn btn-light text-dark btn-sm  btn-icon-text btn-icon-prepend"> <i class="mdi mdi-arrow-left btn-icon-prepend"></i>Voltar </a>
+<a href="<?= site_url("admin/usuarios/show/{$usuario->id}"); ?>"
+    class="btn btn-light text-dark   btn-icon-text btn-icon-prepend"> <i
+        class="mdi mdi-arrow-left  btn-icon-prepend"></i>Voltar </a>
