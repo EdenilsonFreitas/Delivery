7e9c50e077a5411341c80ffb86c45b0318d07efe
Edenilson <freitaseddie126@gmail.com>
2024-09-16 15:33:12 -0300

    Trabalhando com validação de formulário

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -8,12 +8,40 @@ class UsuarioModel extends Model
 {
     protected $table            = 'usuarios';
     protected $returnType       = 'App\Entities\Usuario';
-    protected $useSoftDeletes   = true;
     protected $allowedFields    = ['nome','email','telefone'];
+    protected $useSoftDeletes   = true;
     protected $useTimestamps    = true;
     protected $createdField     = 'criado_em';
     protected $updateField      = 'atualizado_em';
     protected $deletedField     = 'deletado_em';
+    protected $validationRules = [
+        'noome'     => 'required|min_length[4]|alpha_numeric_space||max_length[120]',
+        'email'        => 'required|valid_email|is_unique[usuarios.email]',
+        'cpf'        => 'required|exact_length[14]|is_unique[usuarios.cpf]',
+        'password'     => 'required|min_length[6]',
+        'password_confirmation' => 'required_with[password]|matches[password]',
+    ];
+    protected $validationMessages = [
+        'nome' => [
+            'required' => 'Esse campo é obrigatório.',
+        ],
+          'email' => [
+            'required' => 'Esse campo é obrigatório.',
+            'is_unique' => 'Desculpe! esse email já existe.',
+        ],
+        'cpf' => [
+            'required' => 'Esse campo é obrigatório.',
+            'is_unique' => 'Desculpe! esse CPF já existe.',
+        ],
+    ];
+
+
+
+
+
+
+
+
 
 
 
