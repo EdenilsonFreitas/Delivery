48715c2a7abad209301c8cb075bf2162b20bedbd
Edenilson <freitaseddie126@gmail.com>
2024-09-27 16:05:03 -0300

    Fazendo modificações no código humanize

--- app/Views/Admin/Usuarios/show.php
+++ app/Views/Admin/Usuarios/show.php
@@ -21,13 +21,13 @@
     <div class="col-lg-6 grid-margin stretch-card">
         <div class="card">
             <div class="card-header bg-primary pb-0 pt-4 ">
-            
-            <h4 class="card-title text-white"> <?php echo esc($titulo); ?></h4>
+
+                <h4 class="card-title text-white"> <?php echo esc($titulo); ?></h4>
 
             </div>
 
             <div class="card-body">
-               
+
 
                 <p class="card-text ">
                     <span class="font-wight-bold">Nome:</span>
@@ -49,30 +49,42 @@
                     <?php echo esc($usuario->is_admin ? 'Adminstrador' : 'Cliente'); ?>
                 </p>
 
-                <p class="card-text ">
-                    <span class="font-wight-bold">Criado:</span>
-                    <?php echo $usuario-> criado_em->humanize(); ?>
+                <p class="card-text">
+                    <span class="font-weight-bold">Criado:</span>
+                   <?php echo $usuario->criado_em ? $usuario->criado_em->format('d/m/Y H:i:s') : 'Não disponível'; ?>
+
+
                 </p>
 
-                <p class="card-text ">
-                    <span class="font-wight-bold">Atualizado:</span>
-                    <?php echo $usuario-> atualizado_em->humanize(); ?>
+                <p class="card-text">
+                    <span class="font-weight-bold">Atualizado:</span>
+                     <?php echo $usuario->atualizado_em ? $usuario->atualizado_em->format('d/m/Y H:i:s') : 'Não disponível'; ?>
+
+
                 </p>
+
+                
 
                 <div class="mt-4">
 
-                <a href="<?= site_url("admin/usuarios/editar/{$usuario->id}"); ?>" class="btn btn-dark btn-sm mr-2 btn-icon-text btn-icon-prepend"> <i class="mdi mdi-pencil btn-icon-prepend"></i>Editar </a>
+                    <a href="<?= site_url("admin/usuarios/editar/{$usuario->id}"); ?>"
+                        class="btn btn-dark btn-sm mr-2 btn-icon-text btn-icon-prepend"> <i
+                            class="mdi mdi-pencil btn-icon-prepend"></i>Editar </a>
 
-                <a href="<?= site_url("admin/usuarios/excluir/{$usuario->id}"); ?>" class="btn btn-danger btn-sm mr-2 btn-icon-text btn-icon-prepend"> <i class="mdi mdi-trash-can btn-icon-prepend"></i>Excluir </a>
+                    <a href="<?= site_url("admin/usuarios/excluir/{$usuario->id}"); ?>"
+                        class="btn btn-danger btn-sm mr-2 btn-icon-text btn-icon-prepend"> <i
+                            class="mdi mdi-trash-can btn-icon-prepend"></i>Excluir </a>
 
-                <a href="<?= site_url("admin/usuarios/{$usuario->id}"); ?>" class="btn btn-light text-dark btn-sm  btn-icon-text btn-icon-prepend"> <i class="mdi mdi-arrow-left btn-icon-prepend"></i>Voltar </a>
-                
+                    <a href="<?= site_url("admin/usuarios/{$usuario->id}"); ?>"
+                        class="btn btn-light text-dark btn-sm  btn-icon-text btn-icon-prepend"> <i
+                            class="mdi mdi-arrow-left btn-icon-prepend"></i>Voltar </a>
+
                 </div>
 
 
-               
 
-                
+
+
             </div>
         </div>
     </div>
