50fb93ebbe51d53a166841c61de9fa448ea1456f
Edenilson <freitaseddie126@gmail.com>
2024-10-06 17:56:44 -0300

    mudando o formulário para check-box

--- app/Views/Admin/Usuarios/form.php
+++ app/Views/Admin/Usuarios/form.php
@@ -2,12 +2,14 @@
 
     <div class="form-group col-md-4">
         <label for="nome">Nome</label>
-        <input type="text" class="form-control" name="nome" id="nome" value="<?php echo old('nome', esc($usuario->nome));?>">
+        <input type="text" class="form-control" name="nome" id="nome"
+            value="<?php echo old('nome', esc($usuario->nome));?>">
     </div>
 
     <div class="form-group col-md-2">
         <label for="cpf">CPF</label>
-        <input type="text" class="form-control cpf" name="cpf" id="cpf" value="<?php echo old('cpf',esc($usuario->cpf));?>">
+        <input type="text" class="form-control cpf" name="cpf" id="cpf"
+            value="<?php echo old('cpf',esc($usuario->cpf));?>">
     </div>
 
     <div class="form-group col-md-3">
@@ -18,7 +20,8 @@
 
     <div class="form-group col-md-3">
         <label for="email">E-mail</label>
-        <input type="email" class="form-control" name="email" id="email" value="<?php echo old('email',esc($usuario->email)); ?>">
+        <input type="email" class="form-control" name="email" id="email"
+            value="<?php echo old('email',esc($usuario->email)); ?>">
     </div>
 
 
@@ -39,48 +42,30 @@
 
     </div>
 
-    <div class="form-group col-md-3">
-        <label for="is_admin">Perfil de acesso</label>
-        <select class="form-control" name="is_admin">
 
-            <?php if($usuario->id):?>
-            <option value="1" <?php echo set_select('is_admin', '1'); ?> <?php echo ($usuario-> is_admin? 'selected' : ''); ?>>Administrador</option>
-            <option value="0" <?php echo set_select('is_admin', '0'); ?> <?php echo (!$usuario-> is_admin ? 'selected' : ''); ?>>Cliente</option>
-            
-
-            <?php else: ?>
-            <option value="1" <?php echo set_select('is_admin', '1'); ?> >Sim</option>
-            <option value="0" <?php echo set_select('is_admin', '0'); ?>  selected="">Não</option>
-
-            <?php endif; ?>
-
-
-        </select>
-
-    </div>
-
-    <div class="form-group col-md-3">
-        <label for="ativo">Ativo</label>
-        <select class="form-control" name="ativo">
-
-            <?php if($usuario->id):?>
-            <option value="1" <?php echo set_select('ativo', '1'); ?> <?php echo ($usuario-> ativo ? 'selected' : ''); ?>>Sim</option>
-            <option value="0" <?php echo set_select('ativo', '0'); ?>  <?php echo (!$usuario-> ativo ? 'selected' : ''); ?>>Não</option>
-            
-
-            <?php else: ?>
-
-            <option value="1" <?php echo set_select('ativo', '1'); ?>  >Sim</option>
-            <option value="0" <?php echo set_select('ativo', '0'); ?>  selected="">Não</option>
-
-            <?php endif; ?>
-
-
-        </select>
-
-    </div>
 
 </div>
+
+<div class="form-check form-check-flat form-check-primary mb-3">
+    <label for= "ativo" class="form-check-label">
+
+        <input type = "hidden" name = "ativo" value = "0">
+
+        <input type="checkbox" class = "form-check-input" id = "ativo" name = "ativo" value = "1" <?php if(old('ativo', $usuario-> ativo)) : ?> checked="" <?php endif; ?> >
+        Ativo
+    </label> 
+</div>
+
+<div class="form-check form-check-flat form-check-primary mb-4">
+    <label for= "is_admin" class="form-check-label">
+
+        <input type = "hidden" name = "is_admin" value = "0">
+
+        <input type="checkbox" class = "form-check-input" id = "is_admin" name = "is_admin" value = "1" <?php if(old('is_admin', $usuario-> is_admin)) : ?> checked="" <?php endif; ?> >
+        Administrador
+    </label> 
+</div>
+
 
 
 
